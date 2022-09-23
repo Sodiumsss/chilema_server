af1d708fc871fd09dddb932cc6539981f84bf078
Sodiums <84286875+Sodiumsss@users.noreply.github.com>
2022-09-23 14:34:08 +0800

    update

--- src/main/java/com/yoyo/chilema_server/controller/UserController.java
+++ src/main/java/com/yoyo/chilema_server/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.yoyo.chilema_server.controller;
 
-import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONObject;
 import com.yoyo.chilema_server.common.R;
 import com.yoyo.chilema_server.pojo.Favor;
@@ -15,7 +14,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Base64;
 
 @RestController
 public class UserController {
@@ -28,8 +26,9 @@ public class UserController {
 
     @PostMapping("/api/user/create")
     @CrossOrigin
-    public R addUser(String info, HttpServletRequest request) {
+    public R createUser(String info) {
         String requestInfo = RequestDataUtils.decodeBase64Info(info);
+
         JSONObject obj = (JSONObject) JsonUtils.getJsonObj(requestInfo, "UserAccount");
         UserAccount userAccount = JSONObject.parseObject(String.valueOf(obj), UserAccount.class);
         JSONObject obj2 = (JSONObject) JsonUtils.getJsonObj(requestInfo, "Favor");
@@ -60,14 +59,14 @@ public class UserController {
 
     @PostMapping("/api/favor/update")
     @CrossOrigin
-    public R updateFavor(String info,HttpServletRequest request) {
+    public R updateFavor(String info) {
         Favor favor = RequestDataUtils.decodeInfo(info, Favor.class);
         return favorService.updateFavor(favor);
     }
 
     @PostMapping("/api/user/update")
     @CrossOrigin
-    public R updateUser(String info,HttpServletRequest request) {
+    public R updateUser(String info) {
         UserAccount userAccount = RequestDataUtils.decodeInfo(info, UserAccount.class);
         return userAccountService.updateUserAccount(userAccount);
     }
