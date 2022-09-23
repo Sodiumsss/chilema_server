a8b87e415fe6a72d511be4e887ec69ea67a2802a
Sodiums <84286875+Sodiumsss@users.noreply.github.com>
2022-09-23 13:12:32 +0800

    update

--- src/main/java/com/yoyo/chilema_server/controller/UserController.java
+++ src/main/java/com/yoyo/chilema_server/controller/UserController.java
@@ -1,10 +1,8 @@
 package com.yoyo.chilema_server.controller;
 
-import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONObject;
 import com.yoyo.chilema_server.common.R;
 import com.yoyo.chilema_server.pojo.UserAccount;
-import com.yoyo.chilema_server.pojo.UserRequestBody;
 import com.yoyo.chilema_server.service.UserAccountService;
 import com.yoyo.chilema_server.utils.JsonUtils;
 import com.yoyo.chilema_server.utils.RequestDataUtils;
@@ -14,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Base64;
 
 @RestController
 public class UserController {
@@ -22,13 +19,13 @@ public class UserController {
     @Autowired
     private UserAccountService userAccountService;
 
-
     @PostMapping("/api/user/create")
     @CrossOrigin
-    public R addUser(String info, HttpServletRequest request) {
+    public R addUser(String info) {
         String requestInfo = RequestDataUtils.decodeBase64Info(info);
         JSONObject obj = (JSONObject) JsonUtils.getJsonObj(requestInfo, "UserAccount");
         UserAccount userAccount = JSONObject.parseObject(String.valueOf(obj), UserAccount.class);
+        System.out.println(userAccount);
         return userAccountService.addUserAccount(userAccount);
     }
 
@@ -40,46 +37,46 @@ public class UserController {
 
     @CrossOrigin
     @PostMapping("/api/user/login")
-    public R login(String info,HttpServletRequest httpServletRequest)
+    public R login(String info)
     {
-        if (!httpServletRequest.getHeader("Login").equals("yoyo!")){return R.error("404 NOTFOUND:(");}
-        if (info==null){return R.error("404 NOTFOUND:)");}
         UserAccount userAccount = RequestDataUtils.decodeInfo(info, UserAccount.class);
         return userAccountService.login(userAccount);
     }
     @CrossOrigin
     @PostMapping("/api/user/forgetPW")
-    public R forgetPW(String info,HttpServletRequest httpServletRequest)
+    public R forgetPW(String info)
     {
-        if (!httpServletRequest.getHeader("forgetPW").equals("yoyo!")){return R.error("404 NOTFOUND:(");}
-        if (info==null){return R.error("404 NOTFOUND:)");}
+        UserAccount RAccount = RequestDataUtils.decodeInfo(info,UserAccount.class);
 
-        byte[] base64decodedBytes = Base64.getDecoder().decode(info);
-        JSONObject jsonObj = JSONObject.parseObject(new String(base64decodedBytes));
-        String username=jsonObj.getString("username");
-        UserAccount account =userAccountService.selectUserAccountByUsername(username);
-        if (account==null)
+        UserAccount SAccount =userAccountService.selectUserAccountByUsername(RAccount.getUsername());
+
+        System.out.println(RAccount);
+        System.out.println(SAccount);
+        if (SAccount==null)
         {
             return R.error("填写错误！");
         }
-//        System.out.println(account);
 
-        Integer schoolId=jsonObj.getInteger("schoolId");
-        Integer birthYear=jsonObj.getInteger("birthYear");
-        String password=jsonObj.getString("password");
-
-        if (schoolId.equals(account.getSchoolId()) && birthYear.equals(account.getBirthYear()))
+        if (RAccount.getSchoolId().equals(SAccount.getSchoolId()))
         {
-            account.setPassword(password);
-//            System.out.println(account);
-            return userAccountService.updateUserAccount(account);
+            if(RAccount.getBirthYear().equals(SAccount.getBirthYear()))
+            {
+                SAccount.setPassword(RAccount.getPassword());
+                return userAccountService.updateUserAccount(SAccount);
+            }
         }
-
 
         return R.error("填写错误！");
     }
 
 
+
+
+    private boolean testConnection(String info,String key,String value,HttpServletRequest httpServletRequest)
+    {
+        if (!httpServletRequest.getHeader(key).equals(value)){return false;}
+        return info != null;
+    }
 
 
 }
