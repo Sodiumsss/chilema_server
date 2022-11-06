337909d449eb4e5d062c842b47847c7db052e19b
Sodiums <84286875+Sodiumsss@users.noreply.github.com>
2022-11-06 17:48:49 +0800

    HollowUpdate

--- src/main/java/com/yoyo/chilema_server/handler/SQLTimeHandler.java
+++ src/main/java/com/yoyo/chilema_server/handler/SQLTimeHandler.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 @Component
-public class HollowThreadHandler implements MetaObjectHandler {
+public class SQLTimeHandler implements MetaObjectHandler {
     @Override
     public void insertFill(MetaObject metaObject) {
         setFieldValByName("createTime", LocalDateTime.now(),metaObject);
